dd775a49eca5fab4307c86bcd333060731ec7f6e
Max Woodworth <max10woodworth@gmail.com>
2021-02-18 01:30:03 -0600

    figured out my issue
    I had the json file hardcoded to the html file so it was skipping over the js file and trying to read it by itself.

--- app.js
+++ app.js
@@ -2,7 +2,41 @@
 //Use sample_values as the values for the bar chart.
 // Use otu_ids as the labels for the bar chart.
 // Use otu_labels as the hovertext for the chart.
+
 d3.json("samples.json").then((data) => {
     console.log(data);
     var samples = data.samples;
 })
+// Use sample_values as the values for the bar chart
+function charts(charts){
+    var output = samples.filter(result => results.id == charts);
+    var outcome = output[0];
+
+    var sample_values = outcome.sample_values;
+    var otu_ids = outcome.otu_ids;
+    var otu_labels = outcome.otu_labels;
+
+    var yaxis = otu_ids.slice(0, 10).map(OID => 'OTU ${OID}').reverse();
+    var boutputs = [
+        {
+            y: yaxis,
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "v",
+    }
+    ];
+    var params = {
+        title: "Craziest Bacteria found",
+        margin: {t: 30, l: 150}
+    };
+    Plotly.newPlot("bar", boutputs, params);
+}
+
+
+
+
+
+
+//Initialize
+//init();
